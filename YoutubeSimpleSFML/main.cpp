b113d80d6d0d36dd8fe4605fe009e3b307331b98
Azeajr <zeajantonio@gmail.com>
2022-02-28 13:35:55 -0500

    add Game class

--- YoutubeSimpleSFML/main.cpp
+++ YoutubeSimpleSFML/main.cpp
@@ -1,54 +1,19 @@
-#include <SFML/Audio.hpp>
-#include <SFML/Graphics.hpp>
-#include <SFML/Network.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
+#include "Game.h"
 #include <iostream>
 
 int main() {
-  /**
-   * @brief Window
-   *
-   */
-  sf::RenderWindow window(sf::VideoMode(640, 480), "My first game",
-                    sf::Style::Titlebar | sf::Style::Close);
-  sf::Event ev;
+  // Init game engine
+  Game game;
 
-  /**
-   * @brief Game Loop
-   *
-   */
-  while (window.isOpen()) {
-    // Event polling
-    while (window.pollEvent(ev)) {
-      switch (ev.type) {
-      case sf::Event::Closed:
-        window.close();
-        break;
-
-      case sf::Event::KeyPressed:
-        if (ev.key.code == sf::Keyboard::Escape) {
-          window.close();
-        }
-        break;
-
-      default:
-        break;
-      }
-    }
-
+  // Game loop
+  while (game.running()) {
     // Update
-
-
+    game.update();
 
     // Render
-    window.clear(sf::Color::Blue); // Clear old frame
-
-    // Draw your game
-
-    window.display(); // Tell the app that window is done drawing
+    game.render();
   }
 
-  // End of 
+  // End of application
   return 0;
 }
